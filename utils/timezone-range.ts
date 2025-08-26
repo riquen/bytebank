f26df68c4deac487596ece244badd9e8f340c096
Henri Nunes <henri.arantes1@gmail.com>
2025-08-25 22:24:57 -0300

    fix: correct date calculation in sinceStartOfNDaysAgoUTC function

--- utils/timezone-range.ts
+++ utils/timezone-range.ts
@@ -6,7 +6,7 @@ export const APP_TZ = 'America/Sao_Paulo'
 export function sinceStartOfNDaysAgoUTC(n: number): Date {
   const nowUtc = new Date()
   const nowZoned = toZonedTime(nowUtc, APP_TZ)
-  const startLocal = startOfDay(subDays(nowZoned, n - 1))
+  const startLocal = startOfDay(subDays(nowZoned, n))
 
   return fromZonedTime(startLocal, APP_TZ)
 }
